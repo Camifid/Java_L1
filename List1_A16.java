5a8c553ecaa35e0d8a8520e782ba42de79018511
Camilli Fidelis <109491441+Camifid@users.noreply.github.com>
2023-05-27 19:42:52 -0300

    Create List1_A16.java

---
+++ List1_A16.java
@@ -0,0 +1,52 @@
+/* Escreva um programa para ler as coordenadas (X,Y) de uma
+ * quantidade indeterminada de pontos no sistema cartesiano. 
+ * Para cada ponto escrever o quadrante a que ele pertence. 
+ * O algoritmo será encerrado quando pelo menos uma de duas 
+ * coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+ * 
+ * Exemplo:
+ * Entrada: 2 2 3 -2 -8 -1 -7 1 0 2
+ * Saída: primeiro quarto terceiro segundo
+ * */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class List1_A16 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x,y;
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		while (x != 0 && y != 0) {
+			
+			if (x > 0 && y > 0) {
+			System.out.println("primeiro");
+			}
+		
+			else if (x < 0 && y > 0) {
+				System.out.println("segundo");
+			}
+		
+			else if (x < 0 && y < 0) {
+				System.out.println("terceiro");
+			}
+			
+			else {
+				System.out.println("quarto");
+			}
+			
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+		sc.close();
+	}
+
+}
